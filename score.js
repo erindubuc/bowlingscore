64aa78465afc1e0971863c848878a06857bf8bd2
Erin Dubuc <erin.dubuc9@gmail.com>
2018-07-30 17:27:58 +0000

    step 1

---
+++ score.js
@@ -0,0 +1,32 @@
+
+const scores=[1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 0, 1, 7, 3, 6, 4, 10, 0, 2, 8, 6 ];
+var sum = 0;
+
+ ( i = 0; i < 10; i+=2;) {
+    console.log(framesList);
+  }
+}
+// const add = (a, b) => a + b;
+// const sum = numbers.reduce(add);
+
+
+
+// function makeFramesList {
+//   for  {
+  
+
+//   console.log(makeFramesList);
+//   }
+  
+// }
+
+
+// make set of frames to add later on
+// function frameList(array)= {
+//   for (var i = 0; i < 10; i+2;) {
+//     scores.splice(0,2);
+//   }
+//   console.log(scores);
+// } 
+
+// scores.forEach(function(frame1, )) {
